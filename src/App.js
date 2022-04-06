972e7e172350c111318da10ec82388fb7a59453b
kajeta <ezikeohaking@gmail.com>
2022-04-06 13:34:58 +0100

    jsxcheckpoint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+        <h1 class="title red">EZIKEOHA KINGSLEY </h1>
+
+        <br />
+
+        <img src="/imageInSrc.jpg" />
+
+        <br />
+
+        <img src="/imageInPublic.jpg" />
+      </div>
+
+      <video width="320" height="240" controls>
+        <source src="myVideo.mp4" type="video/mp4" />
+      </video>
     </div>
   );
 }
